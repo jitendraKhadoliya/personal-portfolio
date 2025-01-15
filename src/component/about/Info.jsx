139f32f030a2187998dc0cf80d56a7b451e8fbf4
Jitendra Khadoliya <jitendrasaini352jjnu@gmail.com>
2025-01-15 12:45:52 +0530

    content Updated cv updated

--- src/component/about/Info.jsx
+++ src/component/about/Info.jsx
@@ -12,7 +12,7 @@ const Info = () => {
       <div className="about__box">
         <i className="bx bx-briefcase-alt about__icon"></i>
         <h3 className="about__title">Completed</h3>
-        <span className="about__subtitle">15 + Projects</span>
+        <span className="about__subtitle">30 + Projects</span>
       </div>
 
       <div className="about__box">
