e4d31b5fc38a2a1327bb58ee4e80aafbae69afbc
Jitendra Khadoliya <jitendrasaini352jjnu@gmail.com>
2025-02-18 21:37:51 +0530

    Ui updated

--- src/component/about/Info.jsx
+++ src/component/about/Info.jsx
@@ -6,7 +6,7 @@ const Info = () => {
       <div className="about__box">
         <i className="bx bx-award about__icon"></i>
         <h3 className="about__title">Experience</h3>
-        <span className="about__subtitle">2.5 + Year Working</span>
+        <span className="about__subtitle">3.5 + Year Working</span>
       </div>
 
       <div className="about__box">
